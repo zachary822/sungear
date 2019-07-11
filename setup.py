d869a6f60e7c8c3ce3cfaea0bbebe6df29ec5027
Zachary Juang <zachary822@me.com>
2019-07-11 17:20:00 -0400

    Fix web resize bug

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='sungear',
-    version='1.0.9',
+    version='1.0.10',
     description='Web based Sungear visualization.',
     long_description=long_description,
     long_description_content_type='text/markdown',
