57fc5581fb934518db0f27ad06275fb195cbe6a4
Zachary Juang <zachary822@me.com>
2019-07-11 16:05:03 -0400

    Fix sungear graph bug

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='sungear',
-    version='1.0.7',
+    version='1.0.8',
     description='Web based Sungear visualization.',
     long_description=long_description,
     long_description_content_type='text/markdown',
