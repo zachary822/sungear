6931d27f72975786f32ca699893ca512bb90ca98
Zachary Juang <zachary822@me.com>
2019-07-10 21:36:10 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='sungear',
-    version='1.0.3',
+    version='1.0.4',
     description='Web based Sungear visualization.',
     long_description=long_description,
     long_description_content_type='text/markdown',
